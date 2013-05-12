64393570e375e5c6f3108c4c3de25e6351c49dc2
CleanSky <zoubingsong@163.com>
2013-05-12 21:00:02 +0800

    send email

---
+++ mail.conf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#Filename: mail.conf.py
+{
+    "default" : {
+        "smtpserver" : "smtp.qq.com",
+        "user" : "1157214098@qq.com",
+        "pwd" : "XXXXXXXXXXX"
+    }
+}
